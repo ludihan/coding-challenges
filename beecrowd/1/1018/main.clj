1a8b2db1d7394bcc2f9895245ed24424418ef3c6
ludihan <65617704+ludihan@users.noreply.github.com>
2024-02-12 22:52:08 -0300

    final 1018 clj

--- beecrowd/1/1018/main.clj
+++ beecrowd/1/1018/main.clj
@@ -5,4 +5,6 @@
           (printf "%d nota(s) de R$ %d,00\n" (quot total c) c)
           (divide-cedulas (rem total c) (subvec cedulas 1)))))
 
-(divide-cedulas (read) [100 50 20 10 5 2 1])
+(def input (read))
+(println input)
+(divide-cedulas input [100 50 20 10 5 2 1])
