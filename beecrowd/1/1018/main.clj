73e9faa7d8b5f9c943201684b2ea43187f01db63
ludihan <65617704+ludihan@users.noreply.github.com>
2024-02-12 22:46:15 -0300

    1018 clj

---
+++ beecrowd/1/1018/main.clj
@@ -0,0 +1,8 @@
+(defn divide-cedulas [total cedulas]
+      (if (not= (count cedulas) 0)
+        (do
+          (def c (first cedulas))
+          (printf "%d nota(s) de %d,00\n" (quot total c) c)
+          (divide-cedulas (rem total c) (subvec cedulas 1)))))
+
+(divide-cedulas (read) [100 50 20 10 5 2 1])
