7172ecad8de7d069b071daa26e49affecd995b43
ludihan <65617704+ludihan@users.noreply.github.com>
2024-06-02 15:43:18 -0300

    bit better

--- leetcode/344.rs
+++ leetcode/344.rs
@@ -1,8 +1,8 @@
 impl Solution {
     pub fn reverse_string(s: &mut Vec<char>) {
-        let size = s.len() - 1;
+        let size = s.len();
         for i in 0..size/2  {
-            s.swap(i, size - i);
+            s.swap(i, size - i - 1);
         }
     }
 }
